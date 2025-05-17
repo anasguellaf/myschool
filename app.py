bc5b4dcd3b9890945f30c80ab4e4fa98e2f40ce8
Guellaf Anas <guellaf.anas@gmail.com>
2025-05-17 19:08:03 +0100

    Adding Teachers Table

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from forms import StudentForm
+from flask import Flask, render_template, flash, redirect, url_for, request
+from forms import StudentForm, TeacherForm
 from peewee import *
 from datetime import datetime
 
@@ -24,9 +24,23 @@ class Student(db.Model):
         database = db
         
 
+class Teacher(db.Model):
+    fullname = CharField()
+    tel = CharField()
+    email = CharField(unique=True)
+    experience = IntegerField()
+    subject = CharField()
+    joining_date = DateTimeField(default=datetime.now, formats='%Y-%m-%d %H:%M:%S')
+    
+    class Meta:
+        database = db
+
+
+
+
 def initialize_database():
     db.connect()
-    db.create_tables([Student])
+    db.create_tables([Student, Teacher])
     db.close()
 
 
@@ -37,21 +51,74 @@ with app.app_context():
 # creating the first route for index page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    student_count = Student.select().count()
+    return render_template('index.html',student_count=student_count)
 
 
 # creating the student route
 @app.route("/student", methods=['GET', 'POST'])
 def student_list():
-    return render_template('student.html')
+    students = Student.select()
+    return render_template('student.html',students=students)
 
 # add new student route
 
-@app.route('/student/new')
+@app.route('/student/new', methods=['GET', 'POST'])
 def add_student():
     # create a variable with StudentForm() value
     form = StudentForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        # when the submit button is pressed
+        fullname = form.fullname.data
+        tel = form.tel.data
+        email = form.email.data
+        student = Student.create(
+            fullname = fullname,
+            tel = tel,
+            email = email,
+        )
+        flash('Student added successfully', 'success')
+        return redirect(url_for('student_list'))    
     return render_template('student_new.html',form=form)
+
+
+
+# Decorator 
+@app.route('/teacher')
+def teachers_list():
+    return render_template('teacher.html')
+
+
+@app.route('/teacher/new', methods=['POST','GET'])
+def add_teacher():
+    form = TeacherForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        
+        # 2) insert new teacher into database
+        
+        Teacher.create(
+            fullname = form.fullname.data,
+            tel = form.tel.data,
+            email = form.email.data,
+            experience = form.experience.data,
+            subject = form.subject.data,
+        )
+        
+        return redirect(url_for('teachers_list'))
+        
+        
+    return render_template('teacher_new.html', form=form)
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, DateField, SubmitField
+from wtforms import StringField, EmailField, DateField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 
@@ -9,3 +9,11 @@ class StudentForm(FlaskForm):
     email = EmailField('Email',validators=[DataRequired(),Email()])
     joining_date = DateField('Join Date',validators=[DataRequired()])
     btn_submit = SubmitField('Add Student')
+    
+class TeacherForm(FlaskForm):
+    fullname = StringField('Teacher Fullname', validators=[DataRequired()])
+    tel = StringField('Teacher Phone Number', validators=[DataRequired()])
+    email = EmailField('Teacher Email', validators=[DataRequired(),Email()])
+    experience = IntegerField('Teacher Experience Years', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    btn_submit = SubmitField('Add Teacher')
